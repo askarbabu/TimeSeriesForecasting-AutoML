b965c48bf3d3526757b8090e43ad3fbc8d6be8df
askar-babu-u <askar@liquidity-capital.com>
2022-07-22 11:51:00 +0400

    created execute.py from train_one_company.py and train_all_companies.py.Updated wrapper.py with function name 'main'

--- tsLQC/execute.py
+++ tsLQC/execute.py
@@ -1,7 +1,40 @@
 import pandas as pd
-from train_one_company import modelling
-from forecasting import forecasting_function
-from user_input import autots_hyperparameter_tuning
+from autots import AutoTS
+from autots_hyperparameter_tuning import hyperparameter_tuning
+from tsLQC.forecasting import forecasting_function
+from tsLQC.user_input import autots_hyperparameter_tuning
+from user_input import metric_weighting, \
+    max_generations, num_validations, models_to_validate
+
+from template_generation import templateGeneration
+df = templateGeneration()
+
+
+def modelling(ts, autots_hyperparameter_tuning=False):
+
+    if autots_hyperparameter_tuning:
+        validation_points, validation_method = hyperparameter_tuning(ts, 12)
+    else:
+        validation_points, validation_method = 4, 'backward'
+
+    model_list = [i for i in df.Model.unique() if (i != 'FBProphet')]
+    model = AutoTS(forecast_length=validation_points,
+                   frequency='infer',
+                   models_to_validate=models_to_validate,
+                   no_negatives=True,
+                   ensemble='simple',
+                   max_generations=max_generations,
+                   num_validations=num_validations,
+                   validation_method=validation_method,
+                   model_list=model_list,
+                   n_jobs=7,
+                   metric_weighting=metric_weighting,
+                   )
+    model = model.import_template(df, method='only')
+    model = model.fit(ts.reset_index(), date_col='Date', value_col='Value', id_col=None)
+
+    return model
+
 
 def train_all_companies(timeseries_input_df):
     forecast_df = pd.DataFrame({'CompanyID': [], 'CompanyName': [], 'Date': [],
--- tsLQC/wrapper.py
+++ tsLQC/wrapper.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from user_input import inputTableLocation, COMPANY_LIST
 from preprocess_input import preprocessing
-from train_all_companies import train_all_companies
+from tsLQC.execute import train_all_companies
 from plot_forecast import plot_forecast
 
 
@@ -10,10 +10,11 @@ timeseries_input_df = pd.read_csv(inputTableLocation, index_col=0)
 timeseries_input_df = preprocessing(timeseries_input_df)
 timeseries_input_df = timeseries_input_df[timeseries_input_df.CompanyName.isin(COMPANY_LIST)]
 
-forecast_df = train_all_companies(timeseries_input_df)
+if __name__ == '__main__':
+    forecast_df = train_all_companies(timeseries_input_df)
 
-for i in timeseries_input_df.CompanyName.unique():
-    try:
-        plot_forecast(timeseries_input_df, forecast_df, i)
-    except:
-        pass
+    for i in timeseries_input_df.CompanyName.unique():
+        try:
+            plot_forecast(timeseries_input_df, forecast_df, i)
+        except:
+            pass
