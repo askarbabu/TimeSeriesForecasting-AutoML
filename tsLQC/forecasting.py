878d1c8cdc28c627bd1b7b26c5d0467ce0165bc6
askar-babu-u <askar@liquidity-capital.com>
2022-08-12 17:24:05 +0400

    taking point_forecast out of if else

--- tsLQC/forecasting.py
+++ tsLQC/forecasting.py
@@ -5,10 +5,10 @@ from tsLQC.prediction_interval import prediction_interval
 def forecasting_function(ts, model):
     model_pred = model.predict(forecast_period, prediction_interval=confidence_interval)
 
+    point_forecast = model_pred.forecast
     if model.best_model_name in models_with_custom_interval:
-        point_forecast = model_pred.forecast
         lower_forecast, upper_forecast = prediction_interval(ts, point_forecast, model)
     else:
-        point_forecast, lower_forecast, upper_forecast = model_pred.forecast, model_pred.lower_forecast, model_pred.upper_forecast
+        lower_forecast, upper_forecast = model_pred.lower_forecast, model_pred.upper_forecast
 
     return point_forecast, lower_forecast, upper_forecast
