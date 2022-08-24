29a8b2e9c7d59288e9250e84d0a80d523a646a03
askar-babu-u <askar@liquidity-capital.com>
2022-08-24 18:11:53 +0400

    Updating outlier treatment module

--- tsLQC/preprocess_input.py
+++ tsLQC/preprocess_input.py
@@ -17,7 +17,7 @@ def preprocessing(timeseries_input_df):
 
 
 def outlier_treatment(ts: pd.DataFrame) -> pd.DataFrame:
-    res = seasonal_decompose(ts.interpolate()['Value'], period=6, two_sided=True, extrapolate_trend=1).resid
+    res = seasonal_decompose(ts.interpolate().iloc[:-6]['Value'], period=6, two_sided=True, extrapolate_trend=1).resid
     res_mean, res_std = res.mean(), res.std()
     outlier_upper_cutoff = res_mean + 3 * res.std()
     outlier_lower_cutoff = res_mean - 3 * res.std()
