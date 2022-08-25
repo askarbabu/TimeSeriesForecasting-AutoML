85357b531eaba03e87c7f2831b5bd728a2a5b518
askar-babu-u <askar@liquidity-capital.com>
2022-08-25 16:27:09 +0400

    Adding error handling technique to prediction interval module

--- tsLQC/prediction_interval.py
+++ tsLQC/prediction_interval.py
@@ -1,6 +1,7 @@
 from tsLQC.constant import z_value, std_dev_estimation_len
 import numpy as np
 import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 def prediction_interval(ts, point_forecast, model):
@@ -11,8 +12,13 @@ def prediction_interval(ts, point_forecast, model):
     back_forecast = model.back_forecast(n_splits='auto').forecast.iloc[-std_dev_estimation_len:]
     actual_values = ts.interpolate().iloc[-std_dev_estimation_len:]
     errors = back_forecast - actual_values
-    std_dev_estimated = errors.values.std()
+    std_dev_estimated_from_fit = errors.values.std()
 
+    std_dev_estimated_from_ts = seasonal_decompose(ts.interpolate()['Value'], period=6, two_sided=True,
+                                                   extrapolate_trend=1).resid.std()
+
+    std_dev_estimated = np.max([i for i in [std_dev_estimated_from_fit, std_dev_estimated_from_ts]
+                                if not (np.isnan(i) or np.isinf(i))])
     new_std = std_dev_estimated * np.sqrt(h * (1 + (h/t)))
 
     lower_forecast = pd.DataFrame(point_forecast['Value'] - z_value * new_std)
