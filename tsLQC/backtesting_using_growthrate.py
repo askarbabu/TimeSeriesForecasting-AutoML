b08b673170854970386e52d2d0846da8f511faeb
askar-babu-u <askar@liquidity-capital.com>
2022-08-31 13:11:40 +0400

    Taking constants out of the function

--- tsLQC/backtesting_using_growthrate.py
+++ tsLQC/backtesting_using_growthrate.py
@@ -5,6 +5,10 @@ from autots import AutoTS
 from typing import Tuple
 
 
+FILL_VALUE = 0.1
+PERIOD = 12
+
+
 def compute_growth(x1: float, x2: float) -> float:
     return (x2 - x1) / x1
 
@@ -23,17 +27,17 @@ def backtesting(ts: Series, backtest_length: int, model: AutoTS) -> Tuple[float,
 
     baseline = temp_df[-backtest_length:].sum()
     cumulative_actual = backtest_df.sum()
-    benchmark = rate_periodized(compute_growth(baseline, cumulative_actual), 12)
+    benchmark = rate_periodized(compute_growth(baseline, cumulative_actual), PERIOD)
 
     logging.info(f'benchmark: {benchmark}')
 
-    temp_df[temp_df <= 0] = 0.1
+    temp_df[temp_df <= 0] = FILL_VALUE
     model = model.fit(temp_df.reset_index(), date_col=DATE_COL, value_col=VALUE_COL, id_col=None)
     prediction = model.predict(FORECAST_PERIOD)
     forecast_autots = prediction.forecast[VALUE_COL]
 
     cumulative_forecasted_autots = forecast_autots.iloc[:backtest_length].sum()
-    forecasted_gr = rate_periodized(compute_growth(baseline, cumulative_forecasted_autots), 12)
+    forecasted_gr = rate_periodized(compute_growth(baseline, cumulative_forecasted_autots), PERIOD)
 
     logging.info(f'forecasted growth rate: {forecasted_gr}')
 
